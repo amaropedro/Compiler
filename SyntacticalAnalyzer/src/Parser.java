8e3c9e265cc546074e5227e05aa4ffd7a7890dc6
Amaro <tpedro.amaro@gmail.com>
2023-06-03 20:38:39 -0300

    creates basis for syntacrical analyzer

---
+++ SyntacticalAnalyzer/src/Parser.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Amaro
+ */
+public class Parser {
+    //private token ou terminalSymbol currentTerminal
+    
+    private void accept( /*(token ou terminalSymbol currentTerminal*/){
+        /*
+        if(currentTerminal == expectedTerminal)
+            currentTerminal = next terminal
+        else
+            syntactic erro
+        */
+    }
+    
+    private void acceptIt(){
+        //currentTerminal = next terminal
+    }
+    
+    private void parseProgram(){
+        accept('program');
+        parseID();
+        accept(';');
+        parseCorpo();
+        accept('.');
+    }
+    
+    private void parseID(){
+    
+    }
+    
+    private void parseCorpo(){
+    
+    }
+}
