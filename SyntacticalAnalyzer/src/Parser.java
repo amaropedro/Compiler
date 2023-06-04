ed00e2baf8e1849aa9cbfe1a0c0ced6340da9eb6
Amaro <tpedro.amaro@gmail.com>
2023-06-04 07:41:33 -0300

    adds some parse methods to parser

--- SyntacticalAnalyzer/src/Parser.java
+++ SyntacticalAnalyzer/src/Parser.java
@@ -8,9 +8,10 @@
  * @author Amaro
  */
 public class Parser {
+    private String currentTerminal;
     //private token ou terminalSymbol currentTerminal
     
-    private void accept( /*(token ou terminalSymbol currentTerminal*/){
+    private void accept(String currentTerminal /*(token ou terminalSymbol currentTerminal*/){
         /*
         if(currentTerminal == expectedTerminal)
             currentTerminal = next terminal
@@ -24,19 +25,66 @@ public class Parser {
     }
     
     private void parseProgram(){
-        accept('program');
-        parseID();
-        accept(';');
+        accept("program");
+        accept("<identifier>");
+        accept(";");
         parseCorpo();
-        accept('.');
-    }
-    
-    private void parseID(){
-        
+        accept(".");
     }
     
     private void parseCorpo(){
+        //(declaraVar ;)*
+        while(currentTerminal == "var"){
+            parseDeclaraVar();
+            accept(";");
+        }
+        parseComandoComposto();
+    }
     
+    private void parseDeclaraVar(){
+        accept("var");
+        accept("<identifier>");
+        //(, <id>)*
+        while(currentTerminal == ","){
+            acceptIt();
+            accept("<identifier>");
+        }
+        accept(":");
+        parseTipoSimples();
+    }
+    
+    private void parseComandoComposto(){
+        accept("begin");
+        //(comando ;)*
+        while(currentTerminal == "<identifier>" || currentTerminal == "if" || 
+                currentTerminal =="while" || currentTerminal == "begin"){
+            parseComando();
+            accept(";");
+        }
+        accept("end");
+    }
+    
+    private void parseComando(){
+        switch (currentTerminal){
+            case "<identifier>":
+                    acceptIt();
+                    accept(":=");
+                    parseExpressao();
+                    break;
+            case "if":
+                //
+                break;
+            case "while":
+                //
+                break;
+            case "begin":
+                parseComandoComposto();
+                break;
+            default:
+                break;
+        }
+                
+        
     }
     
 }
